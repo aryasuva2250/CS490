bc84a913f92419e1f1ed5a6e7c138afd71d7b5f1
aryasuva2250 <70666659+aryasuva2250@users.noreply.github.com>
2022-06-01 00:09:27 -0400

    Update backend_login.php

--- backend_login.php
+++ backend_login.php
@@ -1,37 +1,35 @@
 <?php
 include ("databaseConnection.php");
 include ("db.php");
-function P($result, $username, $password) {
-        $st = 'invalid';
-        while($row = $result->fetch_assoc()){
-                 if ($user == $row['Username'] and password_verify($password, $row['Password'])){
-                        $st = $row['Status'];
-                        $c = True;
+if(isset($_POST["username"]) && isset($_POST["passwd"])){
+        $username = $_POST['username'];
+        $password = $_POST['passwd'];
+        if(!empty($username) && !empty($password)){
+                $result = mysqli_query($conn, "SELECT * FROM `USER` WHERE username = '$username'");
+                if($result){
+                        $row = mysqli_fetch_assoc($result);
+                        if($password == $row['passwd']){
+                                $ves = array('type'=>$row['type'], "login"=>1);
+                                $j = json_encode($ves);
+                                echo $j;
+                                return $j;
+                        }
+                        else{
+                                $ves = array("login"=>0);
+                                $j=json_encode($ves);
+                                echo $j;
+                                return $j;
+
+                        }
+                }
+                else{
+                        $ves = array("login"=>0);
+                        $j=json_encode($ves);
+                        echo $j;
+                        return $j;
                 }
         }
-        return $st;
-}
-$s = file_get_contents("php://input");
-$res = json_decode($s, true);
-$username = "";
-$password = "";
-if(isset($res['username']) && isset($res['password'])){
-        $username = $res['username'];
-        $password = $res['password'];
-}
-$result = mysqli_query($conn, "SELECT * FROM `USER`");
-$v = P($result, $username, $password);
-if($v!=null){
-        $ves = array('role'=>$v, 'login'=>true);
-        $j = json_encode($ves);
-        echo $j;
-        return $j;
-}
-else{
-        $ves = array("login"=>false);
-        $j=json_encode($ves);
-        echo $j;
-        return $j;
 }
 $conn->close();
 ?>
+
